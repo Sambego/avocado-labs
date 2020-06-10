b122a01d2ec13c197e3de3cb885325c688166177
Sambego <sambellen@gmail.com>
2020-06-10 10:40:53 +0200

    Support multiple allowed domains

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,11 +4,11 @@ import * as helmet from 'helmet';
 import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
 dotenv.config();
-
+console.log('----', process.env.ALLOWED_ORIGINS.split(','));
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.ALLOWED_ORIGINS,
+    origin: process.env.ALLOWED_ORIGINS.split(','),
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
