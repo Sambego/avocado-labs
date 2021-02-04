374e6701e4b45ea5a00ee64b4f23df20f092bb39
Sambego <sambellen@gmail.com>
2021-02-04 14:16:21 +0100

    Handle special characters in slugs

--- client/gatsby-node.js
+++ client/gatsby-node.js
@@ -4,10 +4,33 @@ const fs = require('fs')
 
 exports.createPages = async({ graphql, actions }) => {
     const { createPage, createRedirect } = actions
-    const toSnakeCase = (string) =>
-        string.replace(/\s-\s/g, '-').replace(/\s/g, '-').toLowerCase()
+    const slugify = (string) => {
+      // Replace all special characters
+        var map = {
+            '-': /(\/|\\|\||\!|\?|\.|\,|\<|\>|\:|\_|\'|\")/,
+            'a': 'á|à|ã|â|À|Á|Ã|Â',
+            'e': 'é|è|ê|É|È|Ê',
+            'i': 'í|ì|î|Í|Ì|Î',
+            'o': 'ó|ò|ô|õ|Ó|Ò|Ô|Õ',
+            'u': 'ú|ù|û|ü|Ú|Ù|Û|Ü',
+            'c': 'ç|Ç',
+            'n': 'ñ|Ñ',
+            'ss': 'ß',
+            // Emoji
+            '': '(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe23\u20d0-\u20f0]|\ud83c[\udffb-\udfff])?(?:\u200d(?:[^\ud800-\udfff]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe23\u20d0-\u20f0]|\ud83c[\udffb-\udfff])?)*'
+        };
+
+        for (var pattern in map) {
+            string = string.replace(new RegExp(map[pattern], 'g'), pattern);
+        };
+
+        // snake-case
+        // Replace all spaces, consecutive dashes, and convert all uppercase to lowercase
+        return string.replace(/(\s-\s)/g, '-').replace(/\s/g, '-').replace(/-{2,}/g).toLowerCase()
+    };
+
     const generateRecordingSlug = (title) =>
-        encodeURIComponent(toSnakeCase(title))
+        encodeURIComponent(slugify(title))
 
     const createEventPages = (events) => {
         const eventPage = path.resolve('./src/templates/event.tsx')
