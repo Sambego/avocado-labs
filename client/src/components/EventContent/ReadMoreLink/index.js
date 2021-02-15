08895620868ad6795bf31ac00d71cfebb34c8655
Sambego <sambellen@gmail.com>
2021-02-15 10:00:11 +0100

    Oh wise log of the console, tell me where this bug hides. I mean it no harm, I just want to talk.

--- client/src/components/EventContent/ReadMoreLink/index.js
+++ client/src/components/EventContent/ReadMoreLink/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ReadMoreLinkStyled from './styled';
 
-const ReadMoreLink = ({ Href, children, center }) => (
-  <ReadMoreLinkStyled href={Href} center={center}>
+const ReadMoreLink = ({ Href, children, center }) => {
+  console.log('--read more link debug, much proffesional!', Href, children);
+  return (<ReadMoreLinkStyled href={Href} center={center}>
     {children}
-  </ReadMoreLinkStyled>
-);
+  </ReadMoreLinkStyled>)
+};
 
 ReadMoreLink.propTypes = {
   Href: PropTypes.node.isRequired,
